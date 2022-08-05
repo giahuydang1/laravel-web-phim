261710ec36dc95af901c04be3aea78823c3c44bc
Huydg <huydg@hybrid-technologies.vn>
2022-08-05 15:58:31 +0700

    crud category

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\HomeController;
+// Admin Controller
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\CountryController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\EpisodeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +21,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'home'])->name('homepage');
+Route::get('/danh-muc', [IndexController::class, 'category'])->name('category');
+Route::get('/the-loai', [IndexController::class, 'genre'])->name('genre');
+Route::get('/quoc-gia', [IndexController::class, 'country'])->name('country');
+Route::get('/phim', [IndexController::class, 'movie'])->name('movie');
+Route::get('/xem-phim', [IndexController::class, 'watch'])->name('watch');
+Route::get('/episode', [IndexController::class, 'episode'])->name('episode');
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// Route Admin
+Route::resource('category', CategoryController::class);
+Route::resource('genre', GenreController::class);
+Route::resource('country', CountryController::class);
+Route::resource('episode', EpisodeController::class);
+Route::resource('movie', MovieController::class);
